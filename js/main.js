7d4b06a20098b28d97b83ff2be5634802b262413
Anton Fedorov <tohafedor@gmail.com>
2020-11-18 02:58:27 +0400

    added aside for catalog section

--- js/main.js
+++ js/main.js
@@ -27,4 +27,76 @@ $(function() {
         nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
     });
 
+    $('.filter-style').styler();
+
+    $('.filter__item-drop, .filter__extra').on('click', function() {
+        $(this).toggleClass('filter__item-drop--active');
+        $(this).next().slideToggle(200);
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        grid: false,
+        hide_min_max: true,
+        hide_from_to: true,
+        onStart: updateInputs,
+        onChange: updateInputs
+    });
+    var instance = $(".js-range-slider").data("ionRangeSlider");
+
+    function updateInputs(data) {
+        var from = $(".js-range-slider").data('from');
+        var to = $(".js-range-slider").data('to');
+
+        $(".js-input-from").prop("value", from);
+        $(".js-input-to").prop("value", to);
+    }
+
+    $(".js-input-from").on("input", function() {
+        var val = $(this).prop("value");
+        var min = $(".js-range-slider").data('min');
+        var to = $(".js-input-to").prop("value", to);
+
+        // validate
+        if (val < min) {
+            val = min;
+        } else
+        if (val > to) {
+            val = to;
+        }
+
+        instance.update({
+            from: val
+        });
+    });
+
+    $(".js-input-to").on("input", function() {
+        var val = $(this).prop("value");
+        var max = $(".js-range-slider").data('max');
+        var from = $(".js-input-from").prop("value", from);
+
+        // validate
+        if (val < from) {
+            val = from;
+        } else if (val > max) {
+            val = max;
+        }
+
+        instance.update({
+            to: val
+        });
+    });
+
+    $('.catalog__filter-btngrid').on('click', function() {
+        $(this).addClass('catalog__filter-button--active');
+        $('.catalog__filter-btnline').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').removeClass('product-item__wrapper--list');
+    });
+
+    $('.catalog__filter-btnline').on('click', function() {
+        $(this).addClass('catalog__filter-button--active');
+        $('.catalog__filter-btngrid').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').addClass('product-item__wrapper--list');
+    });
+
 });
